d0e85cf4a8f1a30600cfffd94d329fbf69791439
LeeSiHyeon <ahah4084@naver.com>
2019-07-17 17:58:39 +0900

    commit

---
+++ public/board_login.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+echo "<button type='button' onclick=button()>ghgß</button>";
+
+
+
+?>
+<html>
+
+<script>
+
+function button() {
+    location.href='board_list.php';
+}
+</script>
+
+
+
